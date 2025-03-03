d559bcd9e5dab9402ce48239dd93766f042ae90c
Jim Solomon <jsolomon@smxtech.com>
2025-03-03 15:59:10 -0500

    Export step 1

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,108 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useRef, useState } from "react";
+import "./App.css";
+import * as xlsx from "xlsx";
+import { Document, ISectionOptions, Packer, Paragraph, TextRun } from "docx";
+import saveAs from "file-saver";
 
-function App() {
-  const [count, setCount] = useState(0)
+export const App = () => {
+    const inputRef = useRef<HTMLInputElement>(null);
+    const [fields, setFields] = useState<
+        Record<string, string>[] | undefined
+    >();
+    const handleFileInput = (event: React.ChangeEvent<HTMLInputElement>) => {
+        const file = event.target.files?.[0];
+        const reader = new FileReader();
+        if (!file) {
+            return;
+        }
+        reader.readAsArrayBuffer(file);
+        reader.onload = () => {
+            const buffer = reader.result as ArrayBuffer;
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+            if (!buffer) {
+                return;
+            }
+            const workbook = xlsx.read(buffer, { type: "buffer" });
+            const sheet = Object.values(workbook.Sheets)[0];
+            const json =
+                xlsx.utils.sheet_to_json<Record<string, string>>(sheet);
+            console.log(Object.keys(json[0]));
+            setFields(json);
+        };
+    };
 
-export default App
+    const convert = () => {
+        if (!fields) {
+            return;
+        }
+        const sections: ISectionOptions[] = fields.map((field) => {
+            const children = Object.entries(field)
+                .filter(([key, value]) => {
+                    if (Number.isInteger(value)) {
+                        const num = parseInt(value);
+                        if (!num) {
+                            return false;
+                        }
+                    }
+                    if (
+                        key.includes("Entry_") ||
+                        key.includes("5050Raffle_Total")
+                    ) {
+                        return false;
+                    }
+                    return true;
+                })
+                .map(([key, value]) => {
+                    let label = key.replace("_", " ");
+                    if (key.includes("_Id")) {
+                        label = "Id";
+                    } else if (key.includes("_Tickets")) {
+                        label = key.substring(0, key.indexOf("_Tickets"));
+                    } else if (key.includes("Name_Last")) {
+                        label = "";
+                    }
+                    label = label.replace("_", " ").trim();
+                    return new TextRun({
+                        text: label ? `${label}: ${value}` : ` ${value}`,
+                        break: key !== "Name_Last" ? 1 : undefined,
+                    });
+                });
+            return {
+                properties: {
+                    type: "nextPage",
+                },
+                children: [
+                    new Paragraph({
+                        children,
+                    }),
+                ],
+            };
+        });
+        const document = new Document({
+            sections,
+        });
+        Packer.toBlob(document).then((blob) => {
+            saveAs(blob, `foo.docx`);
+        });
+    };
+
+    return (
+        <>
+            <button
+                onClick={() => {
+                    inputRef.current?.click();
+                }}
+            >
+                Browse
+            </button>
+
+            <input
+                hidden
+                ref={inputRef}
+                type="file"
+                onChange={handleFileInput}
+            />
+            {fields && <button onClick={convert}>convert</button>}
+        </>
+    );
+};
